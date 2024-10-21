ec60b1f4d79ccdde0c38fbded507a6eae556376e
engineer-adam <engineer@adamlabs.id>
2024-10-21 10:18:34 +0700

    add functionality to serch user by name

--- src/repositories/user-repository.js
+++ src/repositories/user-repository.js
@@ -5,17 +5,16 @@ import { RoleModel, UserModel } from "@adameds/model-sdk/datamaster";
 
 export default class UserRepository {
   static async create(user) {
-    return await sequelizeInstance.transaction(async tr => {
-      return await UserModel.create(user, 
-        { 
-          transaction: tr,
-          returning: true
-        });
+    return await sequelizeInstance.transaction(async (tr) => {
+      return await UserModel.create(user, {
+        transaction: tr,
+        returning: true,
+      });
     });
   }
 
-  static async update(user){
-    return sequelizeInstance.transaction(async tr => {
+  static async update(user) {
+    return sequelizeInstance.transaction(async (tr) => {
       const { uuid } = user;
       const userUpdate = await UserModel.update(user, {
         where: {
@@ -23,190 +22,233 @@ export default class UserRepository {
             { uuid },
             {
               deletedAt: {
-                [Op.is]: null
-              }
-            }
-          ]
+                [Op.is]: null,
+              },
+            },
+          ],
         },
-        transaction: tr
+        transaction: tr,
       });
-      return userUpdate[0]
-    })
+      return userUpdate[0];
+    });
   }
 
   static async countUserByUuid(uuid) {
-    return await sequelizeInstance.transaction(async tr => {
+    return await sequelizeInstance.transaction(async (tr) => {
       return await UserModel.count({
-        where: { 
+        where: {
           [Op.and]: [
             { uuid },
             {
               deletedAt: {
-                [Op.is]: null
-              }
-            }
-          ]
+                [Op.is]: null,
+              },
+            },
+          ],
         },
-        transaction: tr
+        transaction: tr,
       });
     });
   }
 
-  static async findAll(page, pageSize, order, role){
+  static async findAll(page, pageSize, order, role, name) {
     const offset = page * pageSize;
     UserModel.hasOne(RoleModel, {
       foreignKey: "uuid",
       sourceKey: "roleUuid",
-      constraints: false
+      constraints: false,
     });
     RoleModel.belongsTo(UserModel, {
       foreignKey: "uuid",
       targetKey: "roleUuid",
-      constraints: false
-    })
-    return await sequelizeInstance.transaction(async tr => {
-      const { count, rows } = await UserModel.findAndCountAll({
-        limit: pageSize,
-        offset: offset,
-        order: [["id", order]],
-        transaction: tr,
-        include: [
-          {
-            model: RoleModel,
-            attributes: ["name"],
-            where: role,
-            required: true
-          }
-        ]
-      });
-      const payload = rows.map(user => {
-        const role = user.toJSON().RoleModel.name;
-        const { 
-          uuid, name, username, email, phone,
-          createdAt, updatedAt, deletedAt,
-          status, faskesUuid
-        } = user;
+      constraints: false,
+    });
+    return await sequelizeInstance.transaction(async (tr) => {
+      if (name) {
+        const { count, rows } = await UserModel.findAndCountAll({
+          limit: pageSize,
+          offset: offset,
+          order: [["id", order]],
+          transaction: tr,
+          where: {
+            name: {
+              [Op.like]: `%${name}%`,
+            },
+          },
+          include: [
+            {
+              model: RoleModel,
+              attributes: ["name"],
+              where: role,
+              required: true,
+            },
+          ],
+        });
+        const payload = rows.map((user) => {
+          const role = user.toJSON().RoleModel.name;
+          const { uuid, name, username, email, phone, createdAt, updatedAt, deletedAt, status, faskesUuid } = user;
+          return {
+            uuid, name, username, email,
+            phone, createdAt, updatedAt,
+            deletedAt, status, faskesUuid,
+            role,
+          };
+        });
         return {
-          uuid, name, username, email, phone,
-          createdAt, updatedAt, deletedAt, status,
-          faskesUuid, role
-        }
-      })
-
-      return {
-        payload,
-        properties: {
-          totalItem: count,
-          totalPage: Math.ceil(count / pageSize),
-          currentPage: page,
-        }
+          payload,
+          properties: {
+            totalItem: count,
+            totalPage: Math.ceil(count / pageSize),
+            currentPage: page,
+          },
+        };
+      }
+      else {
+        const { count, rows } = await UserModel.findAndCountAll({
+          limit: pageSize,
+          offset: offset,
+          order: [["id", order]],
+          transaction: tr,
+          include: [
+            {
+              model: RoleModel,
+              attributes: ["name"],
+              where: role,
+              required: true,
+            },
+          ],
+        });
+        const payload = rows.map((user) => {
+          const role = user.toJSON().RoleModel.name;
+          const { uuid, name, username, email, phone, createdAt, updatedAt, deletedAt, status, faskesUuid } = user;
+          return {
+            uuid, name, username, email,
+            phone, createdAt, updatedAt,
+            deletedAt, status, faskesUuid,
+            role,
+          };
+        });
+        return {
+          payload,
+          properties: {
+            totalItem: count,
+            totalPage: Math.ceil(count / pageSize),
+            currentPage: page,
+          },
+        };
       }
     });
   }
 
-  static async findByUuid(uuid){
-    return await sequelizeInstance.transaction(async tr => {
-      const user =  await UserModel.findOne({
-        where: {
-          [Op.and]: [
-            { uuid },
-            {
-              deletedAt: {
-                [Op.is]: null
-              }
-            }
-          ]
-        },
-        transaction: tr,
-        attributes: ["uuid", ["role_uuid", "roleUuid"], "name", "username", "email", "phone", ["created_at", "createdAt"], ["updated_at", "updatedAt"]]
-      });
-      return user;
-    });
-  }
-
-  static async findByUsername(username) {
-    try{
-      const result = await sequelizeInstance.transaction(async tr => {
-        return await UserModel.findOne({
-          where: { username },
-          transaction: tr,
-          attributes: ["uuid", ["role_uuid", "roleUuid"], "name", "username", "email", "phone", ["created_at", "createdAt"], ["updated_at", "updatedAt"]]
-        });
-      });
-      return result.get();
-    }catch(error){
-      throw error;
-    }
-  }
-
-  static async findAllDeleted() {
-    const result = await sequelizeInstance.transaction(async tr => {
-      return await UserModel.findAll({
-        where: { 
-          deletedAt: {
-            [Op.not]: null
-          }
-        },
-        transaction: tr,
-        attributes: ["uuid", ["role_uuid", "roleUuid"], "name", "username", "email", "phone", ["created_at", "createdAt"], ["updated_at", "updatedAt"], ["deleted_at", "deletedAt"]]
-      });
-    });
-    return result !== null ? result.map(userDeleted => userDeleted.toJSON()) : result;
-  }
-
-  static async updatePassword(password, uuid){
-    return await sequelizeInstance.transaction(async tr => {
-      const afectedRow = await UserModel.update({password}, {
-        where: {
-          [Op.and]: [
-            {uuid},
-            {
-              deletedAt: {
-                [Op.is]: null
-              }
-            }
-          ]
-        },
-        transaction: tr,
-        returning: ["username"]
-      });
-      return afectedRow[1][0];
-    });
-  }
-
-  static async delete(uuid){
-    const result = await sequelizeInstance.transaction(async tr => {
-      const afectedRow = await UserModel.update({deletedAt: toEpochDate(new Date())}, {
-        where: {
-          [Op.and]: [
-            { uuid },
-            {
-              deletedAt: {
-                [Op.is]: null
-              }
-            }
-          ]
-        },
-        transaction: tr,
-        returning: ["username"]
-      });
-      return afectedRow[0];
-    });
-    return result;
-  }
-
-  static async findByUuidIncludeRole(uuid){
-    return await sequelizeInstance.transaction(async tr => {
+  static async findByUuid(uuid) {
+    return await sequelizeInstance.transaction(async (tr) => {
       const user = await UserModel.findOne({
         where: {
           [Op.and]: [
             { uuid },
             {
               deletedAt: {
-                [Op.is]: null
-              }
-            }
-          ]
+                [Op.is]: null,
+              },
+            },
+          ],
+        },
+        transaction: tr,
+        attributes: ["uuid", ["role_uuid", "roleUuid"], "name", "username", "email", "phone", ["created_at", "createdAt"], ["updated_at", "updatedAt"]],
+      });
+      return user;
+    });
+  }
+
+  static async findByUsername(username) {
+    try {
+      const result = await sequelizeInstance.transaction(async (tr) => {
+        return await UserModel.findOne({
+          where: { username },
+          transaction: tr,
+          attributes: ["uuid", ["role_uuid", "roleUuid"], "name", "username", "email", "phone", ["created_at", "createdAt"], ["updated_at", "updatedAt"]],
+        });
+      });
+      return result.get();
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  static async findAllDeleted() {
+    const result = await sequelizeInstance.transaction(async (tr) => {
+      return await UserModel.findAll({
+        where: {
+          deletedAt: {
+            [Op.not]: null,
+          },
+        },
+        transaction: tr,
+        attributes: ["uuid", ["role_uuid", "roleUuid"], "name", "username", "email", "phone", ["created_at", "createdAt"], ["updated_at", "updatedAt"], ["deleted_at", "deletedAt"]],
+      });
+    });
+    return result !== null ? result.map((userDeleted) => userDeleted.toJSON()) : result;
+  }
+
+  static async updatePassword(password, uuid) {
+    return await sequelizeInstance.transaction(async (tr) => {
+      const afectedRow = await UserModel.update(
+        { password },
+        {
+          where: {
+            [Op.and]: [
+              { uuid },
+              {
+                deletedAt: {
+                  [Op.is]: null,
+                },
+              },
+            ],
+          },
+          transaction: tr,
+          returning: ["username"],
+        }
+      );
+      return afectedRow[1][0];
+    });
+  }
+
+  static async delete(uuid) {
+    const result = await sequelizeInstance.transaction(async (tr) => {
+      const afectedRow = await UserModel.update(
+        { deletedAt: toEpochDate(new Date()) },
+        {
+          where: {
+            [Op.and]: [
+              { uuid },
+              {
+                deletedAt: {
+                  [Op.is]: null,
+                },
+              },
+            ],
+          },
+          transaction: tr,
+          returning: ["username"],
+        }
+      );
+      return afectedRow[0];
+    });
+    return result;
+  }
+
+  static async findByUuidIncludeRole(uuid) {
+    return await sequelizeInstance.transaction(async (tr) => {
+      const user = await UserModel.findOne({
+        where: {
+          [Op.and]: [
+            { uuid },
+            {
+              deletedAt: {
+                [Op.is]: null,
+              },
+            },
+          ],
         },
         attributes: ["name", "email"],
         transaction: tr,
@@ -217,11 +259,11 @@ export default class UserRepository {
             association: UserModel.belongsTo(RoleModel, {
               foreignKey: "roleUuid",
               targetKey: "uuid",
-            })
-          }
-        ]
-      })
+            }),
+          },
+        ],
+      });
       return user;
-    })
+    });
   }
 }
