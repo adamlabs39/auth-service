8c356062f233ce3e0da8e5fd160ec3b4a18d1854
alliano <allianoanonymous@gmail.com>
2024-11-04 14:11:00 +0700

    enable syc db

--- src/app.js
+++ src/app.js
@@ -27,11 +27,11 @@ redisClient.on("connect", () => console.log("Redis alredy accept request"));
 
 app.listen(APPLICATION_PORT, APPLICATION_HOST, async () => {
   await redisClient.connect();
-  // await FaskesModel.sync({ force: true });
-  // await RoleModel.sync({ force: true });
-  // await UserModel.sync({ force: true });
-  // await PegawaiModel.sync({ force: true });
-  // await PractitionerModel.sync({ force: true });
+  await FaskesModel.sync({ force: true });
+  await RoleModel.sync({ force: true });
+  await UserModel.sync({ force: true });
+  await PegawaiModel.sync({ force: true });
+  await PractitionerModel.sync({ force: true });
   await initSueprAdmin();
   await initAdmin();
   console.log(`The server running on http://${APPLICATION_HOST}:${APPLICATION_PORT}`);
