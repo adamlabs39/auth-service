34f40441011c71848499dcabebbc24a4f4aed8e1
engineer-adam <engineer@adamlabs.id>
2025-07-17 15:23:38 +0700

    update package model sdk

--- src/app.js
+++ src/app.js
@@ -7,17 +7,30 @@ import redisClient from "./configurations/redis-client-config.js";
 import requestResponseFormatterMidddleware from "./middlewares/request-response-formatter-middleware.js";
 import morgan from "morgan";
 import cors from "cors";
-import { FaskesModel, PegawaiModel, PractitionerModel, RoleModel, UserModel } from "@adameds/model-sdk/datamaster";
+import {
+  FaskesModel,
+  PegawaiModel,
+  PractitionerModel,
+  RoleModel,
+  UserModel,
+} from "@adameds/model-sdk/datamaster";
 import { initAdmin, initSueprAdmin } from "./seeders/db-seed.js";
 
 const APPLICATION_PORT = process.env.APPLICATION_PORT;
 const APPLICATION_HOST = process.env.APPLICATION_HOST;
-const BASE_URL = `/${process.env.API_BASE ?? "api"}/${process.env.API_VERSION ?? "v3"}/${process.env.APPLICATION_MODULE ?? "auth"}`;
+const BASE_URL = `/${process.env.API_BASE ?? "api"}/${
+  process.env.API_VERSION ?? "v3"
+}/${process.env.APPLICATION_MODULE ?? "auth"}`;
 
 const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(cors({ origin: "*", methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"] }));
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+  })
+);
 app.use(express.urlencoded({ extended: true }));
 app.use(requestResponseFormatterMidddleware);
 app.use(BASE_URL, publicRoutes);
@@ -35,7 +48,9 @@ app.listen(APPLICATION_PORT, APPLICATION_HOST, async () => {
   // await PractitionerModel.sync({ force: true });
   // await initSueprAdmin();
   // await initAdmin();
-  console.log(`The server running on http://${APPLICATION_HOST}:${APPLICATION_PORT}`);
+  console.log(
+    `The server running on http://${APPLICATION_HOST}:${APPLICATION_PORT}`
+  );
 });
 
 export default app;
